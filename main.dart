4c66d048da9d056c968a3677c9764944198b84d3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-03 11:14:57 -0700

    change direction the file

--- main.dart
+++ main.dart
@@ -1,25 +1,12 @@
-import 'dart:io';
-import 'test.dart';
+import 'test.dart' show human;
 
 void main() {
-  // my creater app operation math whit dart
-  var numberone = 0, numbertwo = 0, operation = 0;
-  print("please enter the frist number: ");
-  numberone = int.parse(stdin.readLineSync()!);
-
-  print("please enter your two number: ");
-  numbertwo = int.parse(stdin.readLineSync()!);
-
-  print("please enter your operation: ");
-  operation = int.parse(stdin.readLineSync()!);
-
-  ChoseOperation(operation, numberone, numbertwo);
-  //this is a simple To-do List app in Dart that allows users to add, remove, and mark tasks as completed.
-  //ToDo_List();
-  //this create programme sugget random keyworld
-  int length = readPositiveIntegerInput(
-    "Please enter your number for keyball: ",
+  human h1 = human(
+    name: "hima",
+    email: "shishtawyhima@gmail.com",
+    phone: "01012472095",
+    height: 20,
+    weight: 1.80,
   );
-  List<String> randomWorld = GenerateRandomWorld(length);
-  print("The random world is: ${randomWorld.join()}");
+  h1.walk();
 }
