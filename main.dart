06601b3c42615e823a5c97764d8e4a22130b42c8
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-04 09:54:48 -0700

    i learn try and catch

--- main.dart
+++ main.dart
@@ -2,16 +2,14 @@
 import 'Calculoator.dart';
 import 'PWS.dart';
 import 'ToDo_list.dart';
-*/
-
 import 'OOP.dart';
+*/
 
 void main() {
   /* 
  print_calculator();
  print_To_Do_List();
  printPWS();
- */
 
   human h1 = human(
     name: "hima",
@@ -25,4 +23,14 @@ void main() {
   print("number of arm is ${h1.numberofarm}");
   print("number of legs is ${h1.numberoflegs}");
   h1.walk();
+ */
+  List<int> number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  try {
+    print(number[10]);
+  }
+  // ex is the exception that is thrown when an error occurs
+  // it is a built-in class in dart that is used to handle exceptions
+  catch (ex) {
+    print("the error is $ex");
+  }
 }
