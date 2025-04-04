2aef06dffcdc672c3632bdb366d4cea5a3adb0ca
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-04 12:31:30 -0700

    abstract and interface

--- OOP.dart
+++ OOP.dart
@@ -17,6 +17,9 @@ class human {
     if (numberofarm <= 2 && numberofarm >= 0) {
       //if the number of arm is less than 2 or greater than 0
       this._numberofarm = numberofarm;
+    } else {
+      Exception exception1 = Exception("number of arm is not valid");
+      throw exception1;
     }
   }
 
@@ -25,6 +28,9 @@ class human {
     if (numberoflegs <= 2 && numberoflegs >= 0) {
       //if the number of legs is less than 2 or greater than 0
       this._numberoflegs = numberoflegs;
+    } else {
+      Exception exception = Exception("number of legs is not valid");
+      throw exception;
     }
   }
 
@@ -84,5 +90,55 @@ class human {
 
   void walk() {
     print("the human is walking");
+  }
+}
+
+class animal {
+  int numberoflibs = 4;
+  String? name;
+
+  animal(String? name) {
+    this.name = name;
+  }
+
+  void eat() {
+    print("eating");
+  }
+}
+
+//this is the class dog that extends the class animal
+//this extends the class animal
+class dog extends animal {
+  dog(super.name);
+  @override
+  void eat() {
+    print("dog eating");
+  }
+
+  void bark() {
+    print("barking");
+  }
+}
+
+class cat extends animal {
+  cat(super.name);
+
+  void meow() {
+    print("meowing");
+  }
+}
+
+class tiger extends animal {
+  tiger(super.name);
+  void roar() {
+    print("roaring");
+  }
+}
+
+class lion extends animal {
+  lion(super.name);
+
+  void roar() {
+    print("roaring");
   }
 }
--- main.dart
+++ main.dart
@@ -2,15 +2,10 @@
 import 'Calculoator.dart';
 import 'PWS.dart';
 import 'ToDo_list.dart';
-import 'OOP.dart';
 */
+import 'OOP.dart';
 
 void main() {
-  /* 
- print_calculator();
- print_To_Do_List();
- printPWS();
-
   human h1 = human(
     name: "hima",
     email: "shishtawyhima@gmail.com",
@@ -18,19 +13,19 @@ void main() {
     height: 20,
     weight: 1.80,
   );
-  h1.numberofarm = 2;
-  h1.numberoflegs = 2;
+  try {
+    h1.numberofarm = 3;
+    h1.numberoflegs = 5;
+  } catch (e) {
+    h1.numberofarm = 2;
+    h1.numberoflegs = 2;
+    print(e);
+  }
   print("number of arm is ${h1.numberofarm}");
   print("number of legs is ${h1.numberoflegs}");
   h1.walk();
- */
-  List<int> number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  try {
-    print(number[10]);
-  }
-  // ex is the exception that is thrown when an error occurs
-  // it is a built-in class in dart that is used to handle exceptions
-  catch (ex) {
-    print("the error is $ex");
-  }
+  animal a1 = animal("dog");
+  a1.eat();
+  dog d1 = dog("dog");
+  d1.eat();
 }
